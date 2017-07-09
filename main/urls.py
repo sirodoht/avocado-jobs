4ae62d5afa765a5dbc125de48a7c9519ca232549
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-09 16:40:15 +0300

    update urls

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     # ex: /
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /jobs/submit/
+    url(r'^submit/$', views.submit, name='submit'),
     # ex: /jobs/5/
-    url(r'^jobs/(?P<listing_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^jobs/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /jobs/5/report/
-    url(r'^(jobs/(?P<listing_id>[0-9]+)/report/$', views.report, name='report'),
+    url(r'^jobs/(?P<listing_id>[0-9]+)/report/$', views.report, name='report'),
 ]
