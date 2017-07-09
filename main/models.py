ee81deecf90e6c28ee928a89d9a63916a80dc8d2
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-09 15:25:42 +0300

    add listing model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Listing(models.Model):
+    pub_date = models.DateTimeField('date published')
+    role_title = models.CharField(max_length=50)
+    role_subtitle = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=50)
+    company_link = models.CharField(max_length=300)
+    company_desc = models.TextField()
+    company_size = models.IntegerField(default=0)
+    company_funding = models.CharField(max_length=50)
+    company_tech = models.CharField(max_length=100)
+    role_desc = models.TextField()
+    role_position = models.CharField(max_length=50)
+    role_tech = models.CharField(max_length=100)
+    role_compensation = models.CharField(max_length=100)
+    apply_link = models.CharField(max_length=300)
