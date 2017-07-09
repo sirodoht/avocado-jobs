a20dd5f5ce6a5aaa1ea3079c27d8ea23ad2f1e89
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-09 16:04:43 +0300

    add main views

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Listing
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    listings_list = Listing.objects.order_by('-pub_date')
+    context = {'listings_list': listings_list}
+    return render(request, 'main/index.html', context)
+
+def detail(request, listing_id):
+    return HttpResponse("You're looking at listing %s." % listing_id)
+
+def report(request, listing_id):
+    return HttpResponse("You're looking at the report of listing %s." % listing_id)
