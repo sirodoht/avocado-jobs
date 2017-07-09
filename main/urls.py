6aa3ada32853b93f656080ff1642ea0bb64d6323
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-09 16:04:31 +0300

    add urls for index, detail and report

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'main'
 urlpatterns = [
+    # ex: /
     url(r'^$', views.index, name='index'),
+    # ex: /jobs/5/
+    url(r'^jobs/(?P<listing_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /jobs/5/report/
+    url(r'^(jobs/(?P<listing_id>[0-9]+)/report/$', views.report, name='report'),
 ]
