a6a1bfadeeb0516d347d675bb0a94809dd25c927
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-11 01:06:43 +0300

    add category enum field

--- main/models.py
+++ main/models.py
@@ -18,5 +18,23 @@ class Listing(models.Model):
     role_compensation = models.CharField(max_length=100, blank=True)
     apply_link = models.CharField(max_length=300, blank=True)
 
+    FRONTEND = 'FE'
+    BACKEND = 'BE'
+    FULLSTACK = 'FS'
+    DEVOPS = 'DO'
+    MOBILE = 'MB'
+    CATEGORY_CHOICES = (
+        (FRONTEND, 'Frontend'),
+        (BACKEND, 'Backend'),
+        (FULLSTACK, 'Fullstack'),
+        (DEVOPS, 'Devops'),
+        (MOBILE, 'Mobile'),
+    )
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY_CHOICES,
+        null=True,
+    )
+
     def __str__(self):
         return self.role_title + " at " + self.company_name
