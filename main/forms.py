8dd8d8b64b626400b5325ee44797706f59539d9b
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-10 18:36:12 +0300

    add labels and help texts on new listing form

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,47 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 from .models import Listing
 
 class ListingForm(ModelForm):
     class Meta:
         model = Listing
-        fields = ['role_title', 'role_subtitle', 'company_name', 'company_link',
-            'company_image', 'company_base', 'company_desc', 'company_size',
-            'company_funding', 'company_tech', 'role_desc', 'role_position',
-            'role_tech', 'role_compensation', 'apply_link']
+        fields = ['role_title', 'company_name', 'company_link', 'company_image',
+            'company_base', 'company_desc', 'company_size', 'company_funding',
+            'company_tech', 'role_desc', 'role_position', 'role_tech',
+            'role_compensation', 'apply_link']
+        widgets = {
+            'company_desc': Textarea(attrs={'cols': 50, 'rows': 3}),
+            'role_desc': Textarea(attrs={'cols': 50, 'rows': 10}),
+        }
+        labels = {
+            'company_desc': 'Company description',
+            'role_desc': 'Role description',
+            'company_link': 'Company URL',
+            'company_desc': 'Company intro',
+            'company_funding': 'Funding',
+            'role_desc': 'Job description',
+            'role_position': 'Position',
+            'role_tech': 'Languages / tools',
+            'role_compensation': 'Compensation',
+        }
+        help_texts = {
+            'role_title': 'eg. Frontend Developer',
+            'company_image': 'Provide a url with your company\'s logo. (optional)',
+            'company_base': 'Where are the company\'s offices located? eg. New York City, USA',
+            'company_link': 'eg. https://github.com',
+            'company_desc': 'What does your company do?',
+            'company_size': 'How many employees does your company have?',
+            'company_funding': 'Has your company received funding? If so, how much? eg. $2m / Series A',
+            'company_tech': 'What technologies and/or languages does your company mainly use? eg. Java, Scala',
+            'role_desc': 'What does this role include? Responsibilities, requirements, benefits, etc.',
+            'role_position': 'Is this a full time and part time role? Is this a remote role? eg. Remote full time',
+            'role_tech': 'What technologies and/or languages is this role concerned with? eg. JavaScript, React',
+            'role_compensation': 'What is the salary and/or equity for this role? eg. $70k - $110k / 0.25 - 0.5%',
+            'apply_link': 'How can someone apply? eg. https://avocado-jobs.workable.com/apply/123',
+        }
+        # error_messages = {
+        #     'role_title': {
+        #         'max_length': "This writer's name is too long.",
+        #     },
+        # }
