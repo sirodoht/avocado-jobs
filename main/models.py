21d05ad3268b014480eaf9fbbfd44985effcfa44
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-10 16:38:28 +0300

    add blank true on all fields on listing model

--- main/models.py
+++ main/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class Listing(models.Model):
-    pub_date = models.DateTimeField('date published')
-    role_title = models.CharField(max_length=50)
-    role_subtitle = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=50)
-    company_link = models.CharField(max_length=300)
-    company_image = models.CharField(max_length=300, null=True, blank=True)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    role_title = models.CharField(max_length=50, blank=True)
+    role_subtitle = models.CharField(max_length=100, blank=True)
+    company_name = models.CharField(max_length=50, blank=True)
+    company_link = models.CharField(max_length=300, blank=True)
+    company_image = models.CharField(max_length=300, blank=True)
     company_base = models.CharField(max_length=100, null=True)
-    company_desc = models.TextField()
-    company_size = models.IntegerField(default=0)
-    company_funding = models.CharField(max_length=50)
-    company_tech = models.CharField(max_length=100)
-    role_desc = models.TextField()
-    role_position = models.CharField(max_length=50)
-    role_tech = models.CharField(max_length=100)
-    role_compensation = models.CharField(max_length=100)
-    apply_link = models.CharField(max_length=300)
+    company_desc = models.TextField(blank=True)
+    company_size = models.IntegerField(blank=True)
+    company_funding = models.CharField(max_length=50, blank=True)
+    company_tech = models.CharField(max_length=100, blank=True)
+    role_desc = models.TextField(blank=True)
+    role_position = models.CharField(max_length=50, blank=True)
+    role_tech = models.CharField(max_length=100, blank=True)
+    role_compensation = models.CharField(max_length=100, blank=True)
+    apply_link = models.CharField(max_length=300, blank=True)
 
     def __str__(self):
         return self.role_title + " at " + self.company_name
