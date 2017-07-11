e53a088a82acaf2852e51ae1284e34246dd50394
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-11 21:38:08 +0300

    add dj database url package

--- avocado/settings.py
+++ avocado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+# https://devcenter.heroku.com/articles/django-app-configuration#database-configuration
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
