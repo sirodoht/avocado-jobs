9bd3fdf7eda06be2ef9e2048217837c62f92cb16
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-12 20:57:42 +0300

    add tag model on admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Listing, Category
+from .models import Listing, Category, Tag
 
 admin.site.register(Listing)
 admin.site.register(Category)
+admin.site.register(Tag)
