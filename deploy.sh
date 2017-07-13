ed6b48263647f8aa66eb994b0a13c5fcf6e727de
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-13 23:40:41 +0300

    add branch safety on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)" != "master" ]; then
+    exit 0
+fi
+
+git push origin master &&
 git checkout dokku &&
 git rebase master &&
 git push origin dokku -f &&
