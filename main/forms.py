ce3f28aaa62bb8d928046ce06dc345401b5a5519
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-14 20:42:50 +0300

    add new fields on listing form

--- main/forms.py
+++ main/forms.py
@@ -14,15 +14,17 @@ class ListingForm(ModelForm):
 
     field_order = ['category', 'role_title', 'company_name', 'company_link',
             'company_image', 'company_base', 'company_desc', 'company_size',
-            'company_funding', 'company_tech', 'role_desc', 'tags', 'role_position',
-            'role_tech', 'role_compensation', 'apply_link', 'poster_email']
+            'company_funding', 'company_tech', 'role_desc', 'tags', 'role_type',
+            'role_remote', 'role_location', 'role_tech', 'role_compensation',
+            'apply_link', 'apply_email', 'poster_email']
 
     class Meta:
         model = Listing
         fields = ['category', 'role_title', 'company_name', 'company_link',
             'company_image', 'company_base', 'company_desc', 'company_size',
-            'company_funding', 'company_tech', 'role_desc', 'role_position',
-            'role_tech', 'role_compensation', 'apply_link', 'poster_email']
+            'company_funding', 'company_tech', 'role_desc', 'role_type',
+            'role_remote', 'role_location', 'role_tech', 'role_compensation',
+            'apply_link', 'apply_email', 'poster_email']
         widgets = {
             'company_desc': Textarea(attrs={'cols': 50, 'rows': 3}),
             'role_desc': Textarea(attrs={'cols': 50, 'rows': 10}),
@@ -39,9 +41,12 @@ class ListingForm(ModelForm):
             'company_funding': 'Funding',
             'company_tech': 'Company stack',
             'role_desc': 'Job description',
-            'role_position': 'Position',
+            'role_type': 'Job type',
+            'role_remote': 'Remote?',
+            'role_location': 'Job location',
             'role_tech': 'Languages / tools',
             'role_compensation': 'Compensation',
+            'apply_email': 'Apply email',
             'poster_email': 'Email',
         }
         help_texts = {
@@ -54,9 +59,11 @@ class ListingForm(ModelForm):
             'company_funding': 'Has your company received funding? If so, how much? eg. $2m / Series A',
             'company_tech': 'What technologies and/or languages does your company mainly use? eg. Java, Scala',
             'role_desc': 'What does this role include? Responsibilities, requirements, benefits, etc.',
-            'role_position': 'Is this a full time and part time role? Is this a remote role? eg. Remote full time',
+            'role_remote': 'Is this a remote position?',
+            'role_location': 'Where will this role work from? eg. London, UK',
             'role_tech': 'What technologies and/or languages is this role concerned with? eg. JavaScript, React',
             'role_compensation': 'What is the salary and/or equity for this role? eg. $70k - $110k / 0.25 - 0.5%',
             'apply_link': 'How can someone apply? eg. https://avocado-jobs.workable.com/apply/123',
-            'poster_email': 'The confirmation, along with an edit link for the posting will be sent here.',
+            'apply_email': 'Enter an email in case this is the prefered way of applying. If both this and an \'apply link\' exist, only the link will appear. (optional)',
+            'poster_email': 'The confirmation, along with an edit link for the posting will be sent at this email.',
         }
