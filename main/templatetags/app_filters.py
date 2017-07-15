a6636140b262b63ce8faaac6c5bfc25ee4165555
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-15 17:46:03 +0300

    add tabindex template filter

---
+++ main/templatetags/app_filters.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def tabindex(value, index):
+    """
+    Add a tabindex attribute to the widget for a bound field.
+    """
+    value.field.widget.attrs['tabindex'] = index
+    return value
