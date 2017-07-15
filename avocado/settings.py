847a84fb501b5d15460e11f53c5020ace4362a1f
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-15 23:46:59 +0300

    Revert "leave secret hardcoded"
    This reverts commit e8c1ae5671b630725e971551f1860d402a05510c.

--- avocado/settings.py
+++ avocado/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("NODEBUG") is None else False
