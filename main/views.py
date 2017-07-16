10e57c706cd224f58e8e323a5b00cecce427a4c4
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-07-16 23:36:33 +0300

    implement submit payment view method

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
+import os, json
+import stripe
+
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -24,6 +27,11 @@ class DetailView(generic.DetailView):
     model = Listing
 
 
+class PreviewView(generic.DetailView):
+    template_name = 'main/preview.html'
+    model = Listing
+
+
 def report(request, listing_id):
     return HttpResponse("You're looking at the report of listing %s." % listing_id)
 
@@ -44,5 +52,38 @@ def submit(request):
 
     return render(request, 'main/submit.html', {'form': form})
 
-def submit_confirm(request, listing_id):
+def submit_payment(request, listing_id):
+    stripe_keys = {}
+    stripe_keys['publishable_key'] = os.environ.get('STRIPE_PUBLISHABLE_KEY', 'pk_test_kkUF6UkQvYT5PpzVxfHzLQLv')
+    stripe_keys['secret_key'] = os.environ.get('STRIPE_SECRET_KEY', 'sk_test_UNVwOYwTV7CkTFzLUhjASR08')
+
+    stripe.api_key = stripe_keys['secret_key']
+
+    try:
+      data = json.loads(request.body.decode('utf-8'))
+    except KeyError:
+      HttpResponseServerError('Malformed data!')
+
+    customer = stripe.Customer.create(
+        email=data['email'],
+        source=data['stripeToken'],
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=29900,
+        currency='usd',
+        description='Job listing for 2 months',
+    )
+
+    if charge['status'] == 'succeeded':
+        return HttpResponse(status=204)
+    else:
+        response = {
+            'status': False,
+            'message': 'Unfortunately, there was an charge error. Please try again.',
+        }
+        return JsonResponse(response, status=400)
+
+def submit_thank(request, listing_id):
     return render(request, 'main/thank-you.html', {'listing_id': listing_id})
