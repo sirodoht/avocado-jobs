902e762b3e25f4b2ff8da6b8ccb53609a2a7b5c6
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-01 17:42:42 +0300

    make many fields required in listing model

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ import shortuuid
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 def generate_uuid() -> str:
@@ -10,7 +11,7 @@ def generate_uuid() -> str:
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, null=True)
+    category_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category_name
@@ -23,23 +24,28 @@ class Listing(models.Model):
         default=generate_uuid,
         editable=False,
     )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
     )
     pub_date = models.DateTimeField('date published', default=timezone.now)
-    poster_email = models.EmailField(blank=True, null=True, default='')
-    role_title = models.CharField(max_length=50, blank=True)
-    company_name = models.CharField(max_length=50, blank=True)
-    company_link = models.CharField(max_length=300, blank=True)
+    poster_email = models.EmailField()
+    role_title = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=50)
+    company_link = models.CharField(max_length=300)
     company_image = models.CharField(max_length=300, blank=True)
-    company_base = models.CharField(max_length=100, blank=True)
-    company_desc = models.TextField(blank=True)
-    company_size = models.IntegerField(null=True, blank=True)
-    company_funding = models.CharField(max_length=50, blank=True)
-    company_tech = models.CharField(max_length=100, blank=True)
-    role_desc = models.TextField(blank=True)
+    company_base = models.CharField(max_length=100)
+    company_desc = models.TextField()
+    company_size = models.IntegerField()
+    company_funding = models.CharField(max_length=50)
+    company_tech = models.CharField(max_length=100)
+    role_desc = models.TextField()
 
     FULLTIME = 'F'
     CONTRACT = 'C'
@@ -50,15 +56,13 @@ class Listing(models.Model):
     role_type = models.CharField(
         choices=ROLE_TYPE_CHOICES,
         max_length=1,
-        blank=True,
     )
 
-    role_remote = models.NullBooleanField()
+    role_remote = models.BooleanField()
     role_location = models.CharField(max_length=50, blank=True)
-    role_tech = models.CharField(max_length=100, blank=True)
-    role_compensation = models.CharField(max_length=100, blank=True)
+    role_tech = models.CharField(max_length=100)
+    role_compensation = models.CharField(max_length=100)
     apply_link = models.CharField(max_length=300, blank=True)
-    apply_email = models.EmailField(blank=True)
 
     def __str__(self):
         return self.role_title + " at " + self.company_name
@@ -68,8 +72,7 @@ class Tag(models.Model):
     tag_name = models.CharField(max_length=50)
     listing = models.ForeignKey(
         Listing,
-        on_delete=models.SET_NULL,
-        null=True,
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
