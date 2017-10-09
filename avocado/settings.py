b96bea8caf45f4eb43f57caefdc3f999e61fecba
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-09 04:52:31 +0300

    remove context processor

--- avocado/settings.py
+++ avocado/settings.py
@@ -69,7 +69,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'main.context_processors.users_processor',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
--- main/context_processors.py
+++
@@ -1,4 +0,0 @@
-def users_processor(request):
-    return {
-        'has_listings': bool(request.user.listing_set.all())
-    }
