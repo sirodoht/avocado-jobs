c9b300b03ff5a4966d69772b78049b5a98ce092a
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-09 16:01:31 +0300

    update context processor

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,4 +1,8 @@
 def users_processor(request):
+    if request.user.is_authenticated():
+        has_listings = request.user.listing_set.exists()
+    else:
+        has_listings = False
     return {
-        'has_listings': request.user.listing_set.exists()
+        'has_listings': has_listings,
     }
