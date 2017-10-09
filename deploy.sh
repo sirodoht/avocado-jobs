0626a62ac1530cc3f9caa126464b6a9f7ba168e7
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-09 16:39:07 +0300

    add deploy echo messages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if [ $BRANCH == "master" ]; then
+    echo "Deployment starting..."
     ssh-keyscan -H avocadojobs.com >> ~/.ssh/known_hosts
     git push dokku@avocadojobs.com:avocadojobs master
+else
+    echo "Not on master, deployment canceled."
 fi
