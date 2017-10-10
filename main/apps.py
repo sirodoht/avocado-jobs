41422a97a941fc0d069727cfdc7f5054dd5d1aa3
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-10 23:59:48 +0300

    init segment

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import analytics
 
 
 class MainConfig(AppConfig):
     name = 'main'
+
+    # https://segment.com/docs/sources/server/python/quickstart/
+    def ready(self):
+        analytics.write_key = 'PQgHtk73z7KmT0ZqK6oiMMH13jXmjGXV'
