884dcd548ec5d670421a5499a5be2be9fbb75efd
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-11-08 15:25:04 +0200

    remove contect preprocessor

--- main/context_processors.py
+++
@@ -1,8 +0,0 @@
-def users_processor(request):
-    if request.user.is_authenticated():
-        has_listings = request.user.listing_set.exists()
-    else:
-        has_listings = False
-    return {
-        'has_listings': has_listings,
-    }
