9b6830673730958cae353099c23848abe4ee19cf
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-11-08 15:26:06 +0200

    remove listing category and tag models from admin
    add application

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-from .models import Listing, Category, Tag
+from .models import Application
 
-admin.site.register(Listing)
-admin.site.register(Category)
-admin.site.register(Tag)
+admin.site.register(Application)
