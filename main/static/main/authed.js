4a0725b89ebb7db04b62938c5243d5e519e8ffc2
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-11-10 03:38:57 +0200

    add authed js script

---
+++ main/static/main/authed.js
@@ -0,0 +1,63 @@
+// get csrf from the html
+function getCsrf() {
+  var inputElems = document.querySelectorAll('input');
+  var csrfToken = '';
+  for (i = 0; i < inputElems.length; ++i) {
+    if (inputElems[i].name === 'csrfmiddlewaretoken') {
+      csrfToken = inputElems[i].value;
+      break;
+    }
+  }
+  return csrfToken;
+};
+
+// remove application from user's
+function removeApplication(event) {
+  event.preventDefault();
+
+  if (!window.confirm('Are you sure you want to remove listing application?')) {
+    return;
+  }
+
+  var inputElems = document.querySelectorAll('input');
+  var csrfToken = getCsrf();
+
+  var idForDeletion = event.target.dataset.id;
+  superagent.post('/applications/' + idForDeletion + '/delete/')
+    .set('X-CSRFToken', csrfToken)
+    .end(function (err, res) {
+      if (err) {
+        console.log('Error at deletion:', err);
+      } else {
+        document.location.reload();
+      }
+    });
+}
+
+function changeStageListen(event) {
+  var csrfToken = getCsrf();
+  var listingId = event.target.parentElement.dataset.id;
+  var newStage = event.target.value;
+  superagent.patch('/applications/')
+    .send({
+      'listing_id': listingId,
+      'stage': newStage,
+    })
+    .set('X-CSRFToken', csrfToken)
+    .end(function superagentEndCallback(err, res) {
+      if (err) {
+        console.log('Failed to update application stage. Error:', err);
+      }
+    });
+}
+
+function initApplicationStage() {
+  // add event listeners to select elems
+  var stageSelectElems = document.getElementsByClassName('submission-stage');
+  var i = 0;
+  for (i = 0; i < stageSelectElems.length; i++) {
+    stageSelectElems[i].addEventListener('change', changeStageListen);
+  }
+}
+
+initApplicationStage()
