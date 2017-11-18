ee208b262ab5b17d69b0a4892a0d4bbb373584d8
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-11-18 22:17:51 +0200

    add loading preact component

---
+++ main/frontend/Loading.js
@@ -0,0 +1,12 @@
+import { h, render, Component } from 'preact';
+
+
+export default class New extends Component {
+  render() {
+    return (
+      <div class="container-content-spinner">
+        <div class="spinner" id="loading" title="Saving..."></div>
+      </div>
+    );
+  }
+}
