3bce944da3bd08ea6d1b8c9b1dcd18d52a2bb878
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-12-08 20:10:44 +0200

    update list item component with todo and filtering

--- main/frontend/ListItem.js
+++ main/frontend/ListItem.js
@@ -65,42 +65,47 @@ export default class ListItem extends Component {
 
   render() {
     return (
-      <div class="list-body-entry">
-        <div class="list-body-entry-detail">
-          <div class="list-body-entry-detail-info">
-            <a class="list-body-entry-detail-info-title" href={this.props.data.link} target="_blank">
-              <strong>{this.props.data.role}</strong>
-              <span class="muted">at</span>
-              <span class="list-body-entry-detail-info-title-company">{this.props.data.company}</span>
-              <span class="list-body-entry-detail-info-title-date" title="Date applied">
-                <div class="list-body-entry-detail-info-title-date-body">
-                  {this.props.data.date}
+      <div>
+        {this.props.visible ||
+          <div class="list-body-entry">
+            <div class="list-body-entry-detail">
+              <div class="list-body-entry-detail-info">
+                <a class="list-body-entry-detail-info-title" href={this.props.data.link} target="_blank">
+                  <strong>{this.props.data.role}</strong>
+                  <span class="muted">at</span>
+                  <span class="list-body-entry-detail-info-title-company">{this.props.data.company}</span>
+                  <span class="list-body-entry-detail-info-title-date" title="Date applied">
+                    <div class="list-body-entry-detail-info-title-date-body">
+                      {this.props.data.date}
+                    </div>
+                  </span>
+                </a>
+                <input type="text" name="salary" class="list-body-entry-detail-info-salary"
+                  title="Salary" onInput={this.handleChange} value={this.state.salary} />
+                <div class="list-body-entry-detail-info-stage" title="Current interview stage">
+                  <select name="stage" class="submission-stage" value={this.state.stage} onChange={this.handleChange}>
+                    <option value="todo">To Do</option>
+                    <option value="initial">No initial response yet</option>
+                    <option value="need">I need to respond</option>
+                    <option value="await">Awaiting response</option>
+                    <option value="scheduled">Interview scheduled</option>
+                    <option value="offer">Got offer</option>
+                    <option value="declined">Declined</option>
+                    <option value="rejected">Got Rejected</option>
+                  </select>
                 </div>
-              </span>
-            </a>
-            <input type="text" name="salary" class="list-body-entry-detail-info-salary"
-              title="Salary" onInput={this.handleChange} value={this.state.salary} />
-            <div class="list-body-entry-detail-info-stage" title="Current interview stage">
-              <select name="stage" class="submission-stage" value={this.state.stage} onChange={this.handleChange}>
-                <option value="initial">No initial response yet</option>
-                <option value="need">I need to respond</option>
-                <option value="await">Awaiting response</option>
-                <option value="scheduled">Interview scheduled</option>
-                <option value="offer">Got offer</option>
-                <option value="declined">Declined</option>
-                <option value="rejected">Got Rejected</option>
-              </select>
+              </div>
+            </div>
+            <div class="list-body-entry-control">
+              <div class="list-body-entry-control-rm" title="Remove job application" onClick={this.handleDelete}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" stroke-linecap="round">
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="list-body-entry-control">
-          <div class="list-body-entry-control-rm" title="Remove job application" onClick={this.handleDelete}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" stroke-linecap="round">
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-          </div>
-        </div>
+        }
       </div>
     );
   }
