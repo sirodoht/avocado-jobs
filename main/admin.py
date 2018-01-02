25e2a2c4b22fcc8059e107ee9dd806f326bd5510
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-03 00:50:40 +0200

    update user admin with new fields

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Application, Analytics
 
 admin.site.register(Application)
 admin.site.register(Analytics)
+
+class AvoAdmin(UserAdmin):
+    list_display = ('id', 'username', 'date_joined', 'last_login',)
+
+admin.site.unregister(User)
+admin.site.register(User, AvoAdmin)
