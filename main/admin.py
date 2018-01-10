71c62011c71a6b3522e1d4ba4901d60f39cb2c01
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-10 05:32:45 +0200

    add reminders on admin

--- main/admin.py
+++ main/admin.py
@@ -2,17 +2,27 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Application, Analytics
+from .models import Application, Analytics, Reminder
 
-admin.site.register(Application)
 
-class AnalyticsAdmin(admin.ModelAdmin):
-    list_display = ('ip', 'querystring', 'created_at',)
-
-admin.site.register(Analytics, AnalyticsAdmin)
-
+# Users
 class AvoAdmin(UserAdmin):
     list_display = ('id', 'username', 'date_joined', 'last_login',)
 
 admin.site.unregister(User)
 admin.site.register(User, AvoAdmin)
+
+
+# Job applications
+admin.site.register(Application)
+
+
+# Analytics
+class AnalyticsAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'querystring', 'created_at',)
+
+admin.site.register(Analytics, AnalyticsAdmin)
+
+
+# Reminders
+admin.site.register(Reminder)
