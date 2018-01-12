2289331706a05d13568a28e38029ef3869fe2679
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-12 04:59:56 +0200

    fix deploy static script

--- deploy-static.sh
+++ deploy-static.sh
@@ -2,8 +2,8 @@
 
 npm start
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
-./set-secrets.sh
+source set-secrets.sh
 
 aws s3 sync avocado/staticfiles/ s3://avocado-jobs/static --exclude "*.DS_Store*" --acl public-read --cache-control max-age=31536000
