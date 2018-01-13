11d52be3aeb79d3199e180718584a6cf02b39832
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-01-13 06:13:00 +0200

    add listing form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
+from .models import Listing
+
 
 class EmailForm(forms.Form):
     """The email form for the login page."""
     email = forms.EmailField(label="Your email address")
+
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['role_title', 'company_title', 'company_url', 'location', 'salary', 'application_link']
